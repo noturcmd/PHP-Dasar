611fdadabcd540a8e8608feb43d47534b3315c6f
always_nt <139093156+noturcmd@users.noreply.github.com>
2024-01-19 11:02:46 +0700

    operator array

--- operator-array.php
+++ operator-array.php
@@ -1,11 +1,28 @@
 <?php
 
+    // $angka1 = array(
+    //     "angka1" => 1,
+    //     "angka2" => 2,
+    //     "angka3" => 3,
+    //     "angka4" => 4,
+    //     "angka5" => 5
+    // );
+    // $angka2 = array(
+    //     "angka6" => 6,
+    //     "angka7" => 7,
+    //     "angka8" => 8,
+    //     "angka9" => 9,
+    //     "angka10" => 10
+    // );
+
+    // $angka3 = $angka1 + $angka2;
+    // var_dump($angka3);
+
     $angka1 = array(
         "angka1" => 1,
         "angka2" => 2,
         "angka3" => 3,
         "angka4" => 4,
-        "angka5" => 5
     );
     $angka2 = array(
         "angka6" => 6,
@@ -17,4 +34,5 @@
 
     $angka3 = $angka1 + $angka2;
     var_dump($angka3);
+
 ?>
